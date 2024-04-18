66c89e14038c3e1d3d2b30252f36a69e252732d4
Weslen B <weslenbreno@gmail.com>
2024-04-18 12:05:46 -0300

    chore: semantic

---
+++ release.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ["master", { name: "develop", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/", "package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github"
+  ],
+  repositoryUrl: "https://github.com/your_repository"
+}
