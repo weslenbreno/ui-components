bfc9b74efafe1508788eb4dd718b73a4a78bbb10
Weslen B <weslenbreno@gmail.com>
2024-04-23 13:03:33 -0300

    fix: branch name

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['master', { name: 'develop', prerelease: true }],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
